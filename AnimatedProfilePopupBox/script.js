553f9f465081d9ce5758b4e2f6cd96013a1732a8
Vaibhavi Suradkar <vaibhavisuradkar105@gmail.com>
2024-06-20 19:15:13 +0530

    ProfilePopupBox

---
+++ AnimatedProfilePopupBox/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById('wrapper')
+
+function openPopup(){
+    popup.classList.add('open-popup')
+}
+
+function closePopup(){
+    popup.classList.remove('open-popup')
+}
